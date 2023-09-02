8eddba039f210e632f6659efe856cea8c95e3a02
Avnish-oP <kavnish1245@gmail.com>
2023-09-02 10:38:32 +0530

    tasks page added

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const app = express();
 const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));;
-
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-});
 let tasks=[];
+app.get("/", (req, res) => {
+    res.render("index.ejs", {newItem: tasks});
+});
+
 app.post("/add", (req, res) => { 
     tasks.push(req.body.item);
     res.render("index.ejs",{newItem: tasks});
-    res.redirect("/");
 });
 
 app.post("/delete", (req, res) => {
@@ -20,6 +19,16 @@ app.post("/delete", (req, res) => {
     tasks.splice(req.body.id,1);
     res.render("index.ejs",{newItem: tasks});
 });
+
+app.get("/tasks", (req, res) => {
+    res.render("tasks.ejs", {newItem: tasks});
+});
+app.post("/delete", (req, res) => {
+    console.log(req.body);
+    tasks.splice(req.body.id,1);
+    res.render("tasks.ejs",{newItem: tasks});
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
